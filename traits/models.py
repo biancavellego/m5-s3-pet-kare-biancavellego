f0be0669f5741767584c078bc50b3a0297b1b36e
user <biancavell@hotmail.com>
2023-04-17 19:51:01 -0300

    feat: tarefa 1 finalizada

--- traits/models.py
+++ traits/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 # Create your models here.
-# traits
 from django.db import models
 
 
 class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     pets = models.ManyToManyField("pets.Pet", related_name="traits")
 
     def __repr__(self) -> str:
-        return f"<Trait: ({self.id}) - name: {self.name}>"
+        return (
+            f"<Trait: ({self.id}) - name: {self.name}- created_at: {self.created_at}>"
+        )
