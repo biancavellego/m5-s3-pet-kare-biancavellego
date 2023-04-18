cccaa1402e9504ad2cee5e5bafb98989729665e7
user <biancavell@hotmail.com>
2023-04-17 21:14:49 -0300

    fix: replaced erb pet_kare diagram with corrections.

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from pets.models import Pet
+
 
 # Create your views here.
+class PetView(APIView):
+    def get(self, request: Request) -> Response:
+        # QuerySet
+        pets = Pet.objects.all()
+
+        return Response({"message": "Get Route"})
+
+    def post(self, request: Request) -> Response:
+        return Response({"message": "Post Route"})
